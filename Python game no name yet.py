81aa1f7dd41793b89bd1a666e6fc7458fd057192
FSTIEW <fstiew@gmail.com>
2023-12-01 21:12:50 +0800

    Update Python game no name yet.py

--- Python game no name yet.py
+++ Python game no name yet.py
@@ -11,9 +11,56 @@
 
 import time
 import sys
+import os
 from time import sleep
-Start="Testing, How are you my friend \n"
+
+#Description of the game
+os.system('cls')
+time.sleep(2)
+print("**********************")
+print("welcome to --- this a small game created by university students")
+time.sleep(3)
+print("It's a word based adventure game")
+time.sleep(3)
+print("Player need to type on their keyboard to play this game")
+time.sleep(3)
+print("Hope you enjoy it!")
+time.sleep(3)
+print("**********************")
+input("Press enter to continue")
+time.sleep(1)
+os.system('cls')
+
+#Typing effect for the intro text
+Start=": How you feel... \n"
+
 for char in Start:
+    sleep(0.2)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+S1=": You have been asleep for quite some time...\n"
+
+for char in S1:
     sleep(0.1)
     sys.stdout.write(char)
     sys.stdout.flush()
+
+time.sleep(3)
+
+S2="You open your eyes and saw an oldman is looking at you\n"
+for char in S2:
+    sleep(0.1)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+time.sleep(3)
+
+S3="You:  Where... Where am I..."
+for char in S3:
+    sleep(0.15)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+
+
