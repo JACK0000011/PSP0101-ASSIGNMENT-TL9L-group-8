d3fc78f396cb1a39163d6673e6b835ae1585ecaa
FSTIEW <fstiew@gmail.com>
2023-12-01 13:35:36 +0800

    Create Python game no name yet.py

---
+++ Python game no name yet.py
@@ -0,0 +1,12 @@
+# *********************************************************
+# Program: YOUR_FILENAME.py
+# Course: PSP0101 PROBLEM SOLVING AND PROGRAM DESIGN
+# Class: TL9L
+# Year: 2023/24 Trimester 1
+# Names: Tiew Fu Siang | Low Zheng Hao | Nicholas Beh Zi Yang
+# IDs: 1221107343 | 1221109384 | 1221106297
+# Emails: 1221107343@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3
+# Phones: 010-3706933| MEMBER_PHONE_2 | MEMBER_PHONE_3
+# *********************************************************
+
+print("Welcome to our game!")
