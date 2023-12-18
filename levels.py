c7c50ed8e526d6710c7803d76b310b189f745047
FSTIEW <fstiew@gmail.com>
2023-12-18 12:16:44 +0800

    numerate row and columns to add tiles for them

---
+++ levels.py
@@ -0,0 +1,27 @@
+import pygame
+from tiles import Tile
+from settings import tile_size
+
+class Level:
+    def __init__(self,level_data,surface):
+        self.display_surface = surface 
+        self.setup_level(level_data)
+    
+    #numerate the row and column into number to show the tile position
+    def setup_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        for row_index,row in enumerate(layout):
+            for col_index,col in enumerate(row):
+                print(f'{row_index},{col_index}:{col}')
+
+                #placing tile according to maps
+                if col == 'X':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                
+                    tile= Tile((x,y),tile_size)
+                    self.tiles.add(tile)
+
+
+    def run(self):
+        self.tiles.draw(self.display_surface)
