a372d61fe2f47863f7411e823f852fb35c620355
nbzy05@gmail.com <nbzy05@gmail.com>
2024-01-22 23:41:48 +0800

    Added assignment requirement

--- Jump.py
+++ Jump.py
@@ -1,12 +1,25 @@
+# *********************************************************
+# Program: Jump.py
+# Course: PSP0101 PROBLEM SOLVING AND PROGRAM DESIGN
+# Class: TL9L
+# Year: 2023/24 Trimester 1
+# Names: Tiew Fu Siang | MEMBER_NAME_2 | MEMBER_NAME_3
+# IDs: 1221107343| MEMBER_ID_2 | MEMBER_ID_3
+# Emails: 1221107343@mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3
+# Phones: 0103706933 | MEMBER_PHONE_2 | MEMBER_PHONE_3
+# *********************************************************
+
+
 import pygame
 from pygame.locals import *
 import pickle
 from os import path
 from pygame import mixer
+import json
 pygame.init()
 
 clock = pygame.time.Clock()
-fps=60
+fps = 60
 screen_width = 1000
 screen_height = 800
 text_screen = pygame.display.set_mode((screen_width , screen_height))
@@ -19,10 +32,11 @@ mixer.music.play(-1)
 mixer.music.set_volume(1.8)
 #define game variables
 main_menu = True
-level=1
+level = 1
 game_over = 0
 tile_size = 50
 max_levels = 10
+player_name = 'DefaultPlayer'#
 
 #setting the image
 back_img = pygame.image.load('pictures/prison2.jpg')
@@ -33,12 +47,15 @@ exit_img = pygame.image.load('pictures/exit.png')
 portal_img = pygame.image.load('pictures/portal.jpg')
 
 #text_font
-text_font = pygame.font.SysFont('Arial',30 )
+text_font = pygame.font.SysFont('Arial',30)
 timer_font = pygame.font.SysFont('Arial',30)
 
+thank_you_message = 'You finally made it.Thank you for playing our game!'
+show_thank_you = False 
+
 def draw_text(text, font, text_col, x, y):
-     img = font.render(text, True, text_col)
-     screen.blit(img, (x, y))
+     i = font.render(text, True, text_col)
+     screen.blit(i, (x, y))
 
      
 def reset_level(level):
@@ -110,6 +127,7 @@ class Player():
           self.velo_y = 0
           self.jump_state = 0
           self.jumped = False
+          self.sprint = False #small error
           self.direction = 0 
 
      #updating the player movement
@@ -198,7 +216,11 @@ class Player():
                          
                     #determine if a player is collide with portal and change his level
                     if pygame.sprite.spritecollide(self,portal_group,False):
-                              game_over = 1
+                              #check if it is the last level
+                              if level == max_levels:
+                                   show_thank_you = True
+                              else:
+                                 game_over = 1
                   #           print('changing level')          
               
                y_threshold = screen_height + 100
@@ -282,11 +304,29 @@ replay_button = Button(screen_width // 2 - 70 , screen_height // 2 + 100 , repla
 play_button = Button(screen_width // 2 - 350 , screen_height // 2 , play_img)
 exit_button = Button(screen_width // 2 + 150 , screen_height // 2 , exit_img) 
 
+#
+def load_highscores():
+     try:
+          with open('highscores.json' , 'r') as file:
+               highscores = json.load(file)
+     except(FileNotFoundError , json.JSONDecodeError):
+          highscores = {}
+     return highscores
+
+#
+def save_highscores(highscores):
+     with open('highscores.json' , 'w') as file:
+          json.dump(highscores , file)
+
+#
+highscores = load_highscores()
+
 run = True
 #variable to control when to display the text
 start_time = pygame.time.get_ticks()
 show_text = False 
-text_start_time = 0 #store time when the text is shown
+#store time when the text is shown
+text_start_time = 0 
 time_different = 0 
 show_timer = False
 
@@ -299,12 +339,14 @@ while run :
            run = False
       if play_button.draw():
            main_menu = False
-           show_text = True #set show_text to true when play is clicked
+           #show text when play is clicked
+           show_text = True 
     else:
          world.draw()
          player.update(game_over)
          portal_group.draw(screen)
           
+          #update the timer if game is going
          if game_over == 0:
               current_time = pygame.time.get_ticks()
               time_different = (current_time - start_time) // 1000
@@ -316,6 +358,7 @@ while run :
             replay_action = replay_button.draw()
 
             if replay_action : 
+               #reset everything
                start_time = pygame.time.get_ticks()
                time_different = 0
                level = 1
@@ -349,16 +392,37 @@ while run :
           "Press 'Shift' to sprint and jump further"
      ]
      for text in Tutorial_text :
-        draw_text(text, text_font, (255, 255, 255), 155, 50 + Tutorial_text.index(text)*40) 
-     #check if 15s have passed since that the text was shown
+        draw_text(text, text_font, (255, 255, 255), 300, 50 + Tutorial_text.index(text)*40) 
+     #check if 5s have passed since that the text was shown
      current_time = pygame.time.get_ticks()
      if current_time - text_start_time >= 5000 :
           show_text = False
          
+    if show_thank_you:
+         #make the word to be the center of the screen
+         text_width, text_height = text_font.size(thank_you_message)
+         text_x = (screen_width - text_width) // 2 
+         text_y = ( screen_height- text_height) // 2 
+         draw_text(thank_you_message, text_font, (255, 255, 255), 300, 300)     
+   
+
+    if game_over == 1 and level == max_levels:
+         if time_different < highscores.get(player_name , float('inf')):
+              highscores[player_name] = time_different
+              print(f'Congrates, {player_name}! , You have reach a new highscores; {time_different}s')
+
+              #
+              save_highscores(highscores)
+         
+              #
+              print("\nHighscores:")
+              for name, score in highscores.items():
+                  print(f'{name}: {score}s')
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run = False           
 
     pygame.display.update()
     
-pygame.quit
+pygame.quit()
